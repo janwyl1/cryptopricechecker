2cca8f579d080ffbde942a38c8a7aa5fe2cab9b5
James Anwyl <ja.anwyl@gmail.com>
2019-11-30 16:03:16 +0000

    Updates

---
+++ precache-manifest.9b5adb5cd63878337c2a5af26a4cd702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14e813794e6fdd93321f4cd39165b97",
+    "url": "/cryptopricechecker/index.html"
+  },
+  {
+    "revision": "888ad3477c7c305db049",
+    "url": "/cryptopricechecker/static/css/main.c570345c.chunk.css"
+  },
+  {
+    "revision": "43c941a720f96893e9ce",
+    "url": "/cryptopricechecker/static/js/2.b70271d8.chunk.js"
+  },
+  {
+    "revision": "888ad3477c7c305db049",
+    "url": "/cryptopricechecker/static/js/main.f35e0aa6.chunk.js"
+  },
+  {
+    "revision": "331e950fc645d1555924",
+    "url": "/cryptopricechecker/static/js/runtime-main.cba54afa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.502523d419e827491640370037990f6b.js"
+  "/cryptopricechecker/precache-manifest.9b5adb5cd63878337c2a5af26a4cd702.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cryptopricechecker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
