b3d5c4014f7bfe782364cffb3d9da98dc70130a6
James Anwyl <ja.anwyl@gmail.com>
2019-11-30 15:08:42 +0000

    Added build folder

---
+++ precache-manifest.b12a307b17c14a3e74ba23ebc83cffe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3952fc0783928dd04ae70a2e19233c2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "296d7caee659f950ea5c",
+    "url": "/static/css/main.c570345c.chunk.css"
+  },
+  {
+    "revision": "43c941a720f96893e9ce",
+    "url": "/static/js/2.b70271d8.chunk.js"
+  },
+  {
+    "revision": "296d7caee659f950ea5c",
+    "url": "/static/js/main.1c081c61.chunk.js"
+  },
+  {
+    "revision": "82c0508396b4ef414f0f",
+    "url": "/static/js/runtime-main.7d30d826.js"
+  }
+]);
